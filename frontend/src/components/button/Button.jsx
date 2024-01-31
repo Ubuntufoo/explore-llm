41a970007ba0d47272e25de3fb2d341d6724e52c
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-01-30 23:33:17 -0500

    added loading Spinner for cards, summarize button

--- frontend/src/components/button/Button.jsx
+++ frontend/src/components/button/Button.jsx
@@ -2,11 +2,13 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-bootstrap/Modal';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function Button({ resetOptionsHxCallback, optionsHistory }) {
 
   const [summary, setSummary] = useState(null);
   const [show, setShow] = useState(false);
+  const [showSpinner, setShowSpinner] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -15,6 +17,7 @@ export default function Button({ resetOptionsHxCallback, optionsHistory }) {
   };
 
   const sendSummary = () => {
+    setShowSpinner(true);
     fetch("/api/summarize", {
       method: 'POST',
       headers: {
@@ -26,18 +29,24 @@ export default function Button({ resetOptionsHxCallback, optionsHistory }) {
       }),
     })
       .then((response) => response.json())
-      .then((data) => setSummary(data))
+      .then((data) => {
+        setShowSpinner(false);
+        setSummary(data);
+      })
       .catch((error) => console.error('Error:', error));
   };
 
   return (
     <div className="text-center">
-      <button
-        type="button" className="btn border border-5 border-dark btn-lg btn-dark w-50 mt-4"
-        onClick={() => { sendSummary(); handleShow(); handleResetClick() }}
-      >
-        Summarize
-      </button>
+      <div className="d-flex justify-content-center align-items-center">
+        <button
+          type="button" className="btn border border-5 border-dark btn-lg btn-dark w-50 mt-4"
+          onClick={() => { sendSummary(); handleShow(); handleResetClick() }}
+        >
+          Summarize
+        </button>
+        {showSpinner && <Spinner animation="grow" variant="dark" className="ms-5" />}
+      </div>
       {summary && (
         <Modal
           centered size="xl" fullscreen='lg-down' className="text-white" show={show} onHide={handleClose}
--- frontend/src/components/cards/Cards.jsx
+++ frontend/src/components/cards/Cards.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import './Cards.css';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function RenderCards({ cardTasks, prefs, primaryGoal, setOptionsHistoryCallback }) {
   const [showModal, setShowModal] = useState(false);
@@ -11,8 +12,7 @@ export default function RenderCards({ cardTasks, prefs, primaryGoal, setOptionsH
   const [cardOptions, setCardOptions] = useState({});
   const [selectedOptions, setSelectedOptions] = useState({});
   const [apiCalled, setApiCalled] = useState([]);
-  console.log("cardOptions object is:", cardOptions)
-  console.log("selectedOptions after box is clicked:", selectedOptions);
+  const [showSpinner, setShowSpinner] = useState(false);
 
   const handleCardClick = (task) => {
     setSelectedCard(task);
@@ -29,6 +29,7 @@ export default function RenderCards({ cardTasks, prefs, primaryGoal, setOptionsH
   };
 
   const getOptions = (task) => {
+    setShowSpinner(true);
     fetch("/api/options", {
       method: 'POST',
       headers: {
@@ -41,10 +42,10 @@ export default function RenderCards({ cardTasks, prefs, primaryGoal, setOptionsH
       }),
     })
       .then((response) => response.json())
-      .then((data) =>
-      // call setCardOptions so it sets task as a new value and the data as the value for that key
+      .then((data) => (
+        setShowSpinner(false),
         setCardOptions((prevState) => ({ ...prevState, [task]: data }))
-      )
+      ))
       .catch((error) => console.error('Error:', error));
   };
 
@@ -89,46 +90,51 @@ export default function RenderCards({ cardTasks, prefs, primaryGoal, setOptionsH
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <h6 className='fst-italic'>Recommended:</h6>
-          <ul className="list-group">
-            <li className="list-group-item border border-0 me-auto">
-              <input
-                className="form-check-input me-1"
-                type="checkbox"
-                // checked is true if selectedOptions includes the first option as a value for the selectedCard key
-                checked={selectedOptions[selectedCard] && selectedOptions[selectedCard].includes(cardOptions && cardOptions[0])}
-                onChange={() => handleCheckboxChange(cardOptions && cardOptions[0], selectedCard)}
-                id="firstCheckbox"
-              />
-              <label
-                className="form-check-label stretched-link ms-3 text-white" htmlFor="firstCheckbox"
-              >
-                {/* if the cardOptions object exists, search cardOptions for the selectedCard and display the first option in its array of option values */}
-                {cardOptions[selectedCard] && cardOptions[selectedCard][0]}
-
-              </label>
-            </li>
-            <hr />
-            <h6 className='fst-italic'>Secondary options:</h6>
-            {/* if cardOptions object exists, display the remaining options that correlates to the selectedCard */}
-            {cardOptions[selectedCard] &&
-              cardOptions[selectedCard].slice(1).map((option, index) => (
-                <li key={index} className="list-group-item border border-0 me-auto">
+          {showSpinner ? (
+            <Spinner animation="grow" variant="dark" />
+          ) : (
+            <>
+              <h6 className='fst-italic'>Recommended:</h6>
+              <ul className="list-group">
+                <li className="list-group-item border border-0 me-auto">
                   <input
                     className="form-check-input me-1"
                     type="checkbox"
-                    checked={selectedOptions[selectedCard] && selectedOptions[selectedCard].includes(option)}
-                    onChange={() => handleCheckboxChange(option, selectedCard)}
-                    id={`checkbox${index}`}
+                    // checked is true if selectedOptions includes the first option as a value for the selectedCard key
+                    checked={selectedOptions[selectedCard] && selectedOptions[selectedCard].includes(cardOptions && cardOptions[0])}
+                    onChange={() => handleCheckboxChange(cardOptions && cardOptions[0], selectedCard)}
+                    id="firstCheckbox"
                   />
                   <label
-                    className="form-check-label stretched-link ms-3 text-white" htmlFor={`checkbox${index}`}
+                    className="form-check-label stretched-link ms-3 text-white" htmlFor="firstCheckbox"
                   >
-                    {option}
+                    {/* if the cardOptions object exists, search cardOptions for the selectedCard and display the first option in its array of option values */}
+                    {cardOptions[selectedCard] && cardOptions[selectedCard][0]}
                   </label>
                 </li>
-              ))}
-          </ul>
+                <hr />
+                <h6 className='fst-italic'>Secondary options:</h6>
+                {/* if cardOptions object exists, display the remaining options that correlates to the selectedCard */}
+                {cardOptions[selectedCard] &&
+                  cardOptions[selectedCard].slice(1).map((option, index) => (
+                    <li key={index} className="list-group-item border border-0 me-auto">
+                      <input
+                        className="form-check-input me-1"
+                        type="checkbox"
+                        checked={selectedOptions[selectedCard] && selectedOptions[selectedCard].includes(option)}
+                        onChange={() => handleCheckboxChange(option, selectedCard)}
+                        id={`checkbox${index}`}
+                      />
+                      <label
+                        className="form-check-label stretched-link ms-3 text-white" htmlFor={`checkbox${index}`}
+                      >
+                        {option}
+                      </label>
+                    </li>
+                  ))}
+              </ul>
+            </>
+          )}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleCloseModal}>
